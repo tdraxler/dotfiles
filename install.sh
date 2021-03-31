d2d1d0e58da0f2a07be85f6ce6f9f66010bfaf3c
tdraxler <twdrax@gmail.com>
2021-03-31 11:27:33 -0700

    Added install script and a couple of config files

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo Installing configuration files...
+
+# Copy dotfiles to the correct directories
+cp .vimrc ~/
+
+# Install plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# The next line is copied from powerline/fonts on github
+# It installs some fonts that are compatible with powerline
+
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+# Create Alacritty configuration file
+mkdir --parents ~/.config/alacritty
+cp alacrity.yml ~/.config/alacritty
+
+echo Finished!
