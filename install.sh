88bc3c3f8d9c846e4ba880d31a4fec81023ef940
Thomas <24766838+tdraxler@users.noreply.github.com>
2022-02-13 20:38:19 -0800

    Update install.sh
    Removing oh-my-zsh installation

--- install.sh
+++ install.sh
@@ -32,7 +32,5 @@ cp alacritty.yml ~/.config/alacritty
 echo Installing zsh
 sudo apt install zsh
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 cp .zshrc ~/
 echo Finished!
